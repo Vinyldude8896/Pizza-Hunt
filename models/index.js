51499178b77bf6cd8ef12c112521dd1950f73ddf
Kevin Reid <kevinnivek@me.com>
2022-08-29 11:49:59 -0400

    heroku fix naming problem

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
-const Pizza = require('./Pizza_new');
-const Comment = require('./Comment_new');
+const Pizza = require('./Pizza');
+const Comment = require('./Comment');
 
 module.exports = { Pizza, Comment };
 
